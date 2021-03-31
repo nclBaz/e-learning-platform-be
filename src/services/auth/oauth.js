9f21a4a9eb56847698def340f25ca0977ce3bc2e
Riccardo Gulin <bazuzu666@gmail.com>
2021-03-31 11:55:21 +0200

    Update oauth.js

--- src/services/auth/oauth.js
+++ src/services/auth/oauth.js
@@ -1,7 +1,7 @@
 const passport = require("passport")
 const GoogleStrategy = require("passport-google-oauth20").Strategy
 
-const UserSchema = require("../users/Schema")
+const UserSchema = require("../users/schema")
 const { authenticate } = require("./tools")
 
 passport.use(
